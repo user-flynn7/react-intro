77b0f6e673a6611b83cf5104e7e1ef262a6327c2
Harsh Wadhwa <uif15657@inwerken.de>
2025-04-17 21:01:29 +0530

    styled components and added tailwind base code

---
+++ Styling React/tailwind/vite.config.js
@@ -0,0 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@components": "/src/components"
+		}
+	}
+});
